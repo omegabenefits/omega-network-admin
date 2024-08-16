dc58f6399960d71f93bef189771a3c2641605305
Israel Curtis <admin@omegabenefits.net>
2024-08-16 03:53:58 -0600

    RELEASE version 1.2.3

--- omega-network-admin.php
+++ omega-network-admin.php
@@ -3,7 +3,7 @@
  *  Plugin Name: OMEGA Network Admin
  *	Plugin URI: https://omegabenefits.net
  *  Description: For Multi-Site Networks only! Organizes site listings for easier management
- *  Version: 1.2.2
+ *  Version: 1.2.3
  *  Author: Omega Benefits
  *	Author URI: https://omegabenefits.net
  *  License: GPL-2.0+
@@ -23,7 +23,7 @@
  );
 
 
-wp_enqueue_style( 'ona-style', plugin_dir_url( __FILE__ ) . 'ona.css', array(), "1.2.1", 'all' );
+wp_enqueue_style( 'ona-style', plugin_dir_url( __FILE__ ) . 'ona.css', array(), "1.2.3", 'all' );
 
 
 /**
